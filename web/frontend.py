a1b4933b0b9f5841fd5b12de19ae1ff7c4d2874e
Antonio <amcbd89@gmail.com>
2019-10-29 18:46:17 +0000

    Since 'daqbrokerfrontend' is now pointing to a branch that contains only the dsitribution of the web app, the static file folder must be changed

--- web/frontend.py
+++ web/frontend.py
@@ -4,4 +4,4 @@ from starlette.staticfiles import StaticFiles
 
 frontend = APIRouter()
 
-frontend.mount("/static", StaticFiles(directory=str(Path(__file__).parent / 'frontend/dist')), name="static")
+frontend.mount("/static", StaticFiles(directory=str(Path(__file__).parent / 'daqbrokerfrontend')), name="static")
