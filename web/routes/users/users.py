3e2cc5db4740ede23778b874e39f713a5f2abcbc
Antonio <amcbd89@gmail.com>
2019-11-15 21:06:12 +0000

    Updated user CRUD routes to account for change in data fetching functions

--- web/routes/users/users.py
+++ web/routes/users/users.py
@@ -9,19 +9,19 @@ from daqbrokerServer.storage import session
 from daqbrokerServer.storage.local_settings import User
 from daqbrokerServer.web.classes import User as UserData
 from daqbrokerServer.web.classes import UserInput
-from daqbrokerServer.storage.utils import get_local_resource, get_local_by_attr, get_local_resources, add_local_resource, delete_local_resource
+from daqbrokerServer.storage.utils import get_local_resources, add_local_resource, delete_local_resource
 
 app = APIRouter()
 
 @app.get("/")
 async def get_users(current_user: User = Depends(get_current_user)):
 	current_user.test_security(level=2)
-	return [user.username for user in get_local_resources(db= session, Resource= User)]
+	return [user.username for user in get_local_resources(db= session, Resource= User).all()]
 
 @app.get("/{username}", response_model=UserData)
 async def get_users(username: str, current_user: User = Depends(get_current_user)):
 	current_user.test_security(level=2)
-	user = get_local_by_attr(db= session, Resource= User, attr_name= "username", attr_val= username)
+	user = get_local_resources(db= session, Resource= User, key_vals= { "username":username }).first()
 	#user = User(db= users_db, username= username).get()
 	if not user:
 		raise HTTPException(
@@ -34,7 +34,7 @@ async def get_users(username: str, current_user: User = Depends(get_current_user
 @app.post("/", response_model=UserData)
 async def add_user(new_user: UserInput, current_user: User = Depends(get_current_user)):
 	current_user.test_security(level=2)
-	user = get_local_by_attr(db= session, Resource= User, attr_name= "username", attr_val= new_user.username)
+	user = get_local_resources(db= session, Resource= User, key_vals={ "username":new_user.username }).first()
 	if user:
 		raise HTTPException(
 			status_code=HTTP_409_CONFLICT,
@@ -47,8 +47,8 @@ async def add_user(new_user: UserInput, current_user: User = Depends(get_current
 async def change_user(username: str, new_data : UserInput, current_user: User = Depends(get_current_user)):
 	current_user.test_security(level=2)
 	to_check = ["username", "email"]
-	users = [user for user in get_local_resources(db= session, Resource= User)]
-	user = get_local_by_attr(db= session, Resource= User, attr_name= "username", attr_val= username)
+	users = [user for user in get_local_resources(db= session, Resource= User).all()]
+	user = get_local_resources(db= session, Resource= User, key_vals={ "username":username }).first()
 	exception = HTTPException(
 		status_code=HTTP_404_NOT_FOUND,
 		detail="User '" + username + "' not found",
@@ -56,7 +56,7 @@ async def change_user(username: str, new_data : UserInput, current_user: User = 
 	)
 	if not user:
 		raise exception
-	for attr in to_check:
+	for attr in to_check: # Maybe put this in another function
 		attr_check = [getattr(user_check, attr) for user_check in users]
 		if getattr(new_data, attr) != getattr(user, attr) and getattr(new_data, attr) in attr_check:
 			status_code=HTTP_409_CONFLICT,
@@ -67,7 +67,7 @@ async def change_user(username: str, new_data : UserInput, current_user: User = 
 @app.delete("/{username}", response_model=UserData)
 async def remove_user(username: str, current_user: User = Depends(get_current_user)):
 	current_user.test_security(level=2)
-	user = get_local_by_attr(db= session, Resource= User, attr_name= "username", attr_val= username)
+	user = get_local_resources(db= session, Resource= User, key_vals={ "username":username }).first()
 	if not user:
 		raise HTTPException(
 			status_code=HTTP_404_NOT_FOUND,
