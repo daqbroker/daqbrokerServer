e4eab0e88781632fa35c9de4d442fcbea2863f3a
Antonio <amcbd89@gmail.com>
2019-11-15 21:01:25 +0000

    Updated local ORM settings to store rdbms connection passwords as base64 strings. Placed testing functions for connection objects to facilitate testing a connection

--- storage/local_settings.py
+++ storage/local_settings.py
@@ -1,6 +1,7 @@
+import base64
 import enum
 
-from sqlalchemy import Column, Integer, String, Enum
+from sqlalchemy import Column, Integer, String, Enum, LargeBinary, create_engine, exc, inspect
 from sqlalchemy.schema import UniqueConstraint
 
 from starlette.status import HTTP_401_UNAUTHORIZED
@@ -33,5 +34,26 @@ class Connection(Base):
 	hostname = Column(String)
 	port = Column(Integer)
 	username = Column(String)
-	password = Column(String)
+	password = Column(LargeBinary)
 	__table_args__ = (UniqueConstraint("type", "hostname", "port", "username", "password", name="_connection_details"),)
+
+	def make_url(self):
+		decoded_pw = base64.b64decode(self.password).decode()
+		return self.type + "://" + self.username + ":" + decoded_pw + "@" + self.hostname + ":" + str(self.port) if not "sqlite" in self.type else True
+
+	def test(self): #SQLite test should be reviewed at a later point
+		if "sqlite" in self.type:
+			return (False, "Local SQLite database settings cannot be changed")
+		try:
+			engine = create_engine(self.make_url())
+			conn = engine.connect()
+			conn.close()
+			return (True, "")
+		except exc.OperationalError as e: # see https://docs.sqlalchemy.org/en/13/errors.html#operationalerror this is a driver error
+			return (False, str(e))
+
+	def dict(self): #Using inspector object - see https://docs.sqlalchemy.org/en/13/core/inspection.html
+		return {c.key: getattr(self, c.key) for c in inspect(self).mapper.column_attrs}
+
+#TODO : Must make a many-to-many relationship between User and Connection objects to handle user access to different databases (admin-only)
+
