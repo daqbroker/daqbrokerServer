c04a81303707183171c3a640eb06b02376ac4b78
Antonio <amcbd89@gmail.com>
2019-11-15 21:03:13 +0000

    Previous functions for fetching data were homogenized into one function that does all the fetching of local data

--- storage/utils.py
+++ storage/utils.py
@@ -4,22 +4,24 @@ from pydantic import BaseModel
 from daqbrokerServer.storage.base import Base
 
 #Getting local resources, this should work for anything, as long as it has an 'id' attribute defined as PK
-def get_local_resource(db: Session, Resource: Base, r_id: int):
-	return db.query(Resource).filter(Resource.id == r_id).first()
-
-def get_local_resources(db: Session, Resource: Base, skip: int = 0, limit: int = 100):
-	return db.query(Resource).offset(skip).limit(limit).all()
-
-def get_local_by_attr(db: Session, Resource: Base, attr_name: str, attr_val):
-	if hasattr(Resource, attr_name):
-
-		return db.query(Resource).filter(getattr(Resource, attr_name) == attr_val).first()
+def get_local_resources(db: Session, Resource: Base, r_id: int = None, key_vals: dict = {}, offset: int = 0, limit: int = -1):
+	query = db.query(Resource)
+	if r_id:
+		return query.filter(Resource.id == r_id).first()
+	else:
+		for attr, val in key_vals.items():
+			if hasattr(Resource, attr):
+				query = query.filter(getattr(Resource, attr) == val)
+		query = query.offset(offset)
+		if limit > 0: 
+			query = query.limit(limit) #Not sure this is over, might need to change to account for offsets, should be handleable
+		return query
 	return None
 
 def add_local_resource(db: Session, Resource: Base, user_input: BaseModel, r_id = None):
 	if r_id:
 		data_dict = user_input.dict()
-		resource = get_local_resource(db= db, Resource= Resource, r_id= r_id)
+		resource = get_local_resources(db= db, Resource= Resource, r_id= r_id)
 		for attr, val in data_dict.items():
 			if not val == None:
 				setattr(resource, attr, val)
