f4091442759c2d00f156915f2416b0b25b138530
Antonio <amcbd89@gmail.com>
2019-11-16 16:37:31 +0000

    Updated user and connection endpoints to handle: 1) Introduction of the 'web.classes' module; 2) Introduction of new utility functions to fetch users and connection; 3) IMPORTANT: Added new endpoints to handle the many-to-many relationship between users and connections, meaning a user can handle its connections and a connection can handle its users. Point 3) Will be useful for the administrator panel of the frontend application.

--- web/routes/connections/connections.py
+++ web/routes/connections/connections.py
@@ -3,13 +3,14 @@ from typing import List
 from fastapi import APIRouter, Depends, HTTPException
 from starlette.status import HTTP_401_UNAUTHORIZED, HTTP_404_NOT_FOUND, HTTP_409_CONFLICT, HTTP_422_UNPROCESSABLE_ENTITY, HTTP_503_SERVICE_UNAVAILABLE
 
-from daqbrokerServer.web.routes.utils import get_current_user, make_url
+from daqbrokerServer.web.routes.utils import get_current_user
 
 from daqbrokerServer.storage import session
 from daqbrokerServer.storage.local_settings import Connection, User
-from daqbrokerServer.web.classes import Connection as ConnectionData
-from daqbrokerServer.web.classes import ConnectionInput
+from daqbrokerServer.web.classes.connection import Connection as ConnectionData, ConnectionInput
+from daqbrokerServer.web.classes.user import User as UserData
 from daqbrokerServer.storage.utils import get_local_resources, add_local_resource, delete_local_resource
+from daqbrokerServer.web.routes.utils import get_connection, get_user
 
 app = APIRouter()
 
@@ -18,27 +19,25 @@ async def get_connections(current_user: User = Depends(get_current_user)):
 	current_user.test_security(level=3)
 	return [connection for connection in get_local_resources(db= session, Resource= Connection).all()]
 
-#The way of making actions over specific connections must be well handled, probably over connection.id
 
 @app.get("/{conn_id}", response_model=ConnectionData)
-async def get_connection(conn_id: int, current_user: User = Depends(get_current_user)):
+async def get_single_connection(conn_id: int, current_user: User = Depends(get_current_user)):
 	current_user.test_security(level=3)
-	connection = get_local_resources(db= session, Resource= Connection, r_id= conn_id)
-	if not connection:
-		raise HTTPException(
-			status_code=HTTP_404_NOT_FOUND,
-			detail="Connection not found",
-			headers={"WWW-Authenticate": "Bearer"},
-		)
+	connection = get_connection(conn_id, HTTP_404_NOT_FOUND, err_msg="Connection with id='" + str(conn_id) + "' not found")
 	return connection
+
+@app.get("/{conn_id}/users", response_model=List[UserData])
+async def get_connection_users(conn_id: int, current_user: User = Depends(get_current_user)):
+	current_user.test_security(level=3)
+	connection = get_connection(conn_id, HTTP_404_NOT_FOUND, err_msg="Connection with id='" + str(conn_id) + "' not found")
+	return [user for user in connection.users]
 
 @app.post("/", response_model=ConnectionData)
 async def add_connection(new_conn: ConnectionInput, current_user: User = Depends(get_current_user)):
 	current_user.test_security(level=3)
 	exception = HTTPException(
 		status_code=HTTP_409_CONFLICT,
-		detail="This connection already exists",
-		headers={"WWW-Authenticate": "Bearer"},
+		detail="This connection already exists"
 	)
 	#Must test connection here
 	key_vals = {attr: getattr(new_conn, attr)  for attr in ["hostname", "port", "type"]}
@@ -48,21 +47,40 @@ async def add_connection(new_conn: ConnectionInput, current_user: User = Depends
 	new_connection = Connection(**new_conn.dict())
 	conn_test = new_connection.test()
 	if conn_test[0]:
-		return add_local_resource(db= session, Resource= Connection, user_input= new_conn)
+		added_conn = add_local_resource(db= session, Resource= Connection, user_input= new_conn)
+		#Adding relationship parameters here - should change add_local_resource to include relationships
+		added_conn.users.append(current_user)
+		session.commit()
+		return added_conn
 	else:
 		exception.status_code = HTTP_503_SERVICE_UNAVAILABLE
 		exception.detail = conn_test[1]
 		raise exception
 
+@app.post("/{conn_id}/users", response_model=List[UserData])
+async def get_connection_users(conn_id: int, usernames: List[str], current_user: User = Depends(get_current_user)):
+	current_user.test_security(level=3)
+	connection = get_connection(conn_id, HTTP_404_NOT_FOUND, err_msg="Connection with id='" + str(conn_id) + "' not found")
+	for username in usernames:
+		user = get_user(username, HTTP_404_NOT_FOUND, err_msg="User '" + username + "' not found")
+		user_exists = [user for user in connection.users if user.username == username]
+		if len(user_exists) < 1: #Should I instead raise an exception here?
+			connection.users.append(user)
+		#raise HTTPException(
+		#	status_code=HTTP_409_CONFLICT,
+		#	detail="Username '" + username + "' already assigned to this connection (id='" + str(conn_id) + "')"
+		#)
+	session.commit()
+	return [user for user in connection.users]
+
 @app.put("/{conn_id}", response_model=ConnectionData)
 async def change_connection(conn_id: int, new_data : ConnectionInput, current_user: User = Depends(get_current_user)):
 	current_user.test_security(level=3)
-	connection = get_local_resources(db= session, Resource= Connection, r_id= conn_id)
+	connection = get_connection(conn_id, HTTP_404_NOT_FOUND, err_msg="Connection with id='" + str(conn_id) + "' not found")
 	connection_dict = connection.dict()
 	exception = HTTPException(
-		status_code=HTTP_404_NOT_FOUND,
-		detail="Connection not found",
-		headers={"WWW-Authenticate": "Bearer"},
+		status_code=HTTP_409_CONFLICT,
+		detail="Attempting to change to already existing connection"
 	)
 	if not connection:
 		raise exception
@@ -73,8 +91,6 @@ async def change_connection(conn_id: int, new_data : ConnectionInput, current_us
 	key_vals = {attr: getattr(new_connection, attr) for attr in ["hostname", "port", "type"]}
 	new_connection_query = get_local_resources(db= session, Resource= Connection, key_vals=key_vals).first()
 	if new_connection_query:
-		exception.status_code = HTTP_409_CONFLICT
-		exception.detail = "Attempting to change to already existing connection"
 		raise exception
 	new_connection_test = new_connection.test()
 	if not new_connection_test[0]:
@@ -86,12 +102,20 @@ async def change_connection(conn_id: int, new_data : ConnectionInput, current_us
 @app.delete("/{conn_id}", response_model=ConnectionData)
 async def remove_connection(conn_id: int, current_user: Connection = Depends(get_current_user)):
 	current_user.test_security(level=3)
-	connection = get_local_resources(db= session, Resource= Connection, r_id= conn_id)
-	if not connection:
-		raise HTTPException(
-			status_code=HTTP_404_NOT_FOUND,
-			detail="User '" + username + "' not found",
-			headers={"WWW-Authenticate": "Bearer"},
-		)
+	connection = get_connection(conn_id, HTTP_404_NOT_FOUND, err_msg="Connection with id='" + str(conn_id) + "' not found")
 	return delete_local_resource(db= session, Resource= Connection, instance= connection)
 
+@app.delete("/{conn_id}/users", response_model=List[UserData])
+async def get_connection_users(conn_id: int, usernames: List[str], current_user: User = Depends(get_current_user)):
+	current_user.test_security(level=3)
+	connection = get_connection(conn_id, HTTP_404_NOT_FOUND, err_msg="Connection with id='" + str(conn_id) + "' not found")
+	to_delete = []
+	for username in usernames:
+		user = get_user(username, HTTP_404_NOT_FOUND, err_msg="User '" + username + "' not found")
+		user_exists = [user for user in connection.users if user.username == username]
+		# Should consider raising an exception here if the user is not found
+		if len(user_exists) > 0:
+			connection.users.remove(user)
+			to_delete.append(user)
+	session.commit()
+	return to_delete
--- web/routes/users/users.py
+++ web/routes/users/users.py
@@ -6,10 +6,11 @@ from starlette.status import HTTP_401_UNAUTHORIZED, HTTP_404_NOT_FOUND, HTTP_409
 from daqbrokerServer.web.routes.utils import get_current_user, Depends, HTTPException, oauth2_scheme
 
 from daqbrokerServer.storage import session
-from daqbrokerServer.storage.local_settings import User
-from daqbrokerServer.web.classes import User as UserData
-from daqbrokerServer.web.classes import UserInput
+from daqbrokerServer.storage.local_settings import User, Connection
+from daqbrokerServer.web.classes.user import User as UserData, UserInput
+from daqbrokerServer.web.classes.connection import Connection as ConnectionData
 from daqbrokerServer.storage.utils import get_local_resources, add_local_resource, delete_local_resource
+from daqbrokerServer.web.routes.utils import get_user, get_connection
 
 app = APIRouter()
 
@@ -19,41 +20,44 @@ async def get_users(current_user: User = Depends(get_current_user)):
 	return [user.username for user in get_local_resources(db= session, Resource= User).all()]
 
 @app.get("/{username}", response_model=UserData)
-async def get_users(username: str, current_user: User = Depends(get_current_user)):
+async def get_single_user(username: str, current_user: User = Depends(get_current_user)):
 	current_user.test_security(level=2)
-	user = get_local_resources(db= session, Resource= User, key_vals= { "username":username }).first()
-	#user = User(db= users_db, username= username).get()
-	if not user:
-		raise HTTPException(
-			status_code=HTTP_404_NOT_FOUND,
-			detail="User " + username + " not found",
-			headers={"WWW-Authenticate": "Bearer"},
-		)
-	return user
+	return get_user(username, HTTP_404_NOT_FOUND, err_msg = "User '" + username + "' not found")
+
+@app.get("/{username}/connections", response_model=List[ConnectionData])
+async def get_user_connections(username: str, current_user: User = Depends(get_current_user)):
+	current_user.test_security(level=2)
+	user = get_user(username, HTTP_404_NOT_FOUND, err_msg = "User '" + username + "' not found")
+	return [connection for connection in user.connections]
 
 @app.post("/", response_model=UserData)
 async def add_user(new_user: UserInput, current_user: User = Depends(get_current_user)):
-	current_user.test_security(level=2)
-	user = get_local_resources(db= session, Resource= User, key_vals={ "username":new_user.username }).first()
-	if user:
-		raise HTTPException(
-			status_code=HTTP_409_CONFLICT,
-			detail="User '" + new_user.username + "' already exists",
-			headers={"WWW-Authenticate": "Bearer"},
-		)
+	current_user.test_security(level=3)
+	user = get_user(new_user.username, HTTP_409_CONFLICT, err_msg="User '" + new_user.username + "' already exists", find=False)
 	return add_local_resource(db= session, Resource= User, user_input= new_user)
+
+@app.post("/{username}/connections", response_model=List[ConnectionData])
+async def add_user_connection(conn_ids: List[int], username: str, current_user: User = Depends(get_current_user)):
+	current_user.test_security(level=3)
+	user = get_user(username, HTTP_404_NOT_FOUND, err_msg = "User '" + username + "' not found")
+	for conn_id in conn_ids:
+		connection  = get_connection(conn_id, HTTP_404_NOT_FOUND, err_msg="Connection with id='" + str(conn_id) + "' not found")
+		conn_exists = [connection for connection in user.connections if connection.id == conn_id]
+		if len(conn_exists) < 1: # Should I instead raise an exception here?
+			user.connections.append(connection)
+		#raise HTTPException(
+		#	status_code=HTTP_409_CONFLICT,
+		#	detail="Connection with id='" + str(conn_id) + "' already exists in user '" + username + "'"
+		#)
+	session.commit()
+	return [connection for connection in user.connections]
 
 @app.put("/{username}", response_model=UserData)
 async def change_user(username: str, new_data : UserInput, current_user: User = Depends(get_current_user)):
-	current_user.test_security(level=2)
+	current_user.test_security(level=3)
 	to_check = ["username", "email"]
 	users = [user for user in get_local_resources(db= session, Resource= User).all()]
-	user = get_local_resources(db= session, Resource= User, key_vals={ "username":username }).first()
-	exception = HTTPException(
-		status_code=HTTP_404_NOT_FOUND,
-		detail="User '" + username + "' not found",
-		headers={"WWW-Authenticate": "Bearer"},
-	)
+	user = get_user(username, HTTP_404_NOT_FOUND, err_msg = "User '" + username + "' not found")
 	if not user:
 		raise exception
 	for attr in to_check: # Maybe put this in another function
@@ -66,13 +70,20 @@ async def change_user(username: str, new_data : UserInput, current_user: User = 
 
 @app.delete("/{username}", response_model=UserData)
 async def remove_user(username: str, current_user: User = Depends(get_current_user)):
-	current_user.test_security(level=2)
-	user = get_local_resources(db= session, Resource= User, key_vals={ "username":username }).first()
-	if not user:
-		raise HTTPException(
-			status_code=HTTP_404_NOT_FOUND,
-			detail="User '" + username + "' not found",
-			headers={"WWW-Authenticate": "Bearer"},
-		)
+	current_user.test_security(level=3)
+	user = get_user(username, HTTP_404_NOT_FOUND, err_msg = "User '" + username + "' not found")
 	return delete_local_resource(db= session, Resource= User, instance= user)
 
+@app.delete("/{username}/connections/", response_model=List[ConnectionData])
+async def remove_user(username: str, conn_ids: List[int], current_user: User = Depends(get_current_user)):
+	current_user.test_security(level=3)
+	user = get_user(username, HTTP_404_NOT_FOUND, err_msg = "User '" + username + "' not found")
+	to_delete = []
+	for conn_id in conn_ids:
+		connection  = get_connection(conn_id, HTTP_404_NOT_FOUND, err_msg="Connection with id='" + str(conn_id) + "' not found")
+		conn_exists = [connection for connection in user.connections if connection.id == conn_id]
+		if len(conn_exists) > 0:
+			to_delete.append(conn_exists[0])
+			user.connections.remove(conn_exists[0])
+	session.commit()
+	return to_delete
