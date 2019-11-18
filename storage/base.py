29932de1ca0504c30ae7a695351516a5f58a9afe
Antonio <amcbd89@gmail.com>
2019-11-18 18:30:14 +0000

    Introduced a declarative base to be used for remote daqbroker databases. Remote is not a good name, it should be used for daqbroker databases and not the local settings database

--- storage/base.py
+++ storage/base.py
@@ -3,5 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 #Declarative base?
 Base = declarative_base()
 
+#RemoteBase
+RemoteBase = declarative_base()
+
 def get_base():
 	return Base
