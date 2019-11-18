85cc117b1247b38c0f570353ecb9b4e70c8a53b0
Antonio <amcbd89@gmail.com>
2019-11-18 18:48:43 +0000

    Updated connection-related routes to include adding/deleting campaigns from a certain connection. TODO: A callable class should be created to apply to the current user to test if said user is assigned to the connection where a campaign action (add/delete) is being attempted

--- web/routes/connections/connections.py
+++ web/routes/connections/connections.py
@@ -3,38 +3,36 @@ from typing import List
 from fastapi import APIRouter, Depends, HTTPException
 from starlette.status import HTTP_401_UNAUTHORIZED, HTTP_404_NOT_FOUND, HTTP_409_CONFLICT, HTTP_422_UNPROCESSABLE_ENTITY, HTTP_503_SERVICE_UNAVAILABLE
 
-from daqbrokerServer.web.routes.utils import get_current_user
-
-from daqbrokerServer.storage import session
-from daqbrokerServer.storage.local_settings import Connection, User
+from daqbrokerServer.web.routes.utils import AuthUser
+from daqbrokerServer.storage.local_schema import Connection, User
 from daqbrokerServer.web.classes.connection import Connection as ConnectionData, ConnectionInput
 from daqbrokerServer.web.classes.user import User as UserData
 from daqbrokerServer.storage.utils import get_local_resources, add_local_resource, delete_local_resource
-from daqbrokerServer.web.routes.utils import get_connection, get_user
+from daqbrokerServer.web.routes.utils import get_connection, get_user, get_db, test_connection, add_campaign, remove_campaign
 
 app = APIRouter()
 
-@app.get("/", response_model=List[ConnectionData])
-async def get_connections(current_user: User = Depends(get_current_user)):
-	current_user.test_security(level=3)
+@app.get("/", response_model=List[ConnectionData], dependencies=[Depends(AuthUser(3))])
+async def get_connections(session= Depends(get_db)):
 	return [connection for connection in get_local_resources(db= session, Resource= Connection).all()]
 
 
-@app.get("/{conn_id}", response_model=ConnectionData)
-async def get_single_connection(conn_id: int, current_user: User = Depends(get_current_user)):
-	current_user.test_security(level=3)
-	connection = get_connection(conn_id, HTTP_404_NOT_FOUND, err_msg="Connection with id='" + str(conn_id) + "' not found")
-	return connection
+@app.get("/{conn_id}", response_model=ConnectionData, dependencies=[Depends(AuthUser(3))])
+async def get_single_connection(conn_id: int, session= Depends(get_db)):
+	return get_connection(session, conn_id, HTTP_404_NOT_FOUND, err_msg="Connection with id='" + str(conn_id) + "' not found")
 
-@app.get("/{conn_id}/users", response_model=List[UserData])
-async def get_connection_users(conn_id: int, current_user: User = Depends(get_current_user)):
-	current_user.test_security(level=3)
-	connection = get_connection(conn_id, HTTP_404_NOT_FOUND, err_msg="Connection with id='" + str(conn_id) + "' not found")
+@app.get("/{conn_id}/users", response_model=List[UserData], dependencies=[Depends(AuthUser(3))])
+async def get_connection_users(conn_id: int, session= Depends(get_db)):
+	connection = get_connection(session, conn_id, HTTP_404_NOT_FOUND, err_msg="Connection with id='" + str(conn_id) + "' not found")
 	return [user for user in connection.users]
 
-@app.post("/", response_model=ConnectionData)
-async def add_connection(new_conn: ConnectionInput, current_user: User = Depends(get_current_user)):
-	current_user.test_security(level=3)
+@app.get("/{conn_id}/campaigns", response_model=List[str], dependencies=[Depends(AuthUser(3))])
+async def get_connection_users(conn_id: int, session= Depends(get_db)):
+	connection = get_connection(session, conn_id, HTTP_404_NOT_FOUND, err_msg="Connection with id='" + str(conn_id) + "' not found")
+	return connection.campaigns
+
+@app.post("/", response_model=ConnectionData, dependencies=[])
+async def add_connection(new_conn: ConnectionInput, session= Depends(get_db), current_user: User = Depends(AuthUser(3))):
 	exception = HTTPException(
 		status_code=HTTP_409_CONFLICT,
 		detail="This connection already exists"
@@ -44,25 +42,20 @@ async def add_connection(new_conn: ConnectionInput, current_user: User = Depends
 	connection = get_local_resources(db= session, Resource= Connection, key_vals=key_vals).first()
 	if connection:
 		raise exception
-	new_connection = Connection(**new_conn.dict())
-	conn_test = new_connection.test()
-	if conn_test[0]:
-		added_conn = add_local_resource(db= session, Resource= Connection, user_input= new_conn)
-		#Adding relationship parameters here - should change add_local_resource to include relationships
-		added_conn.users.append(current_user)
-		session.commit()
-		return added_conn
-	else:
-		exception.status_code = HTTP_503_SERVICE_UNAVAILABLE
-		exception.detail = conn_test[1]
-		raise exception
+	new_connection = Connection(**new_conn.dict()) # This object only wastes memory, should there be a way to send the object over 'add_local_resource' as 'user_input'
+	test_connection(new_connection)
+	added_conn = add_local_resource(db= session, Resource= Connection, user_input= new_conn)
+	added_conn.setup()
+	# Adding relationship parameters here - should change add_local_resource to include relationships
+	added_conn.users.append(current_user) # Currrently doesn't work because there is no instance of the User class representing the logged user
+	session.commit()
+	return added_conn
 
-@app.post("/{conn_id}/users", response_model=List[UserData])
-async def get_connection_users(conn_id: int, usernames: List[str], current_user: User = Depends(get_current_user)):
-	current_user.test_security(level=3)
-	connection = get_connection(conn_id, HTTP_404_NOT_FOUND, err_msg="Connection with id='" + str(conn_id) + "' not found")
+@app.post("/{conn_id}/users", response_model=List[UserData], dependencies=[Depends(AuthUser(3))])
+async def add_users_to_connection(conn_id: int, usernames: List[str], session= Depends(get_db)):
+	connection = get_connection(session, conn_id, HTTP_404_NOT_FOUND, err_msg="Connection with id='" + str(conn_id) + "' not found")
 	for username in usernames:
-		user = get_user(username, HTTP_404_NOT_FOUND, err_msg="User '" + username + "' not found")
+		user = get_user(session, username, HTTP_404_NOT_FOUND, err_msg="User '" + username + "' not found")
 		user_exists = [user for user in connection.users if user.username == username]
 		if len(user_exists) < 1: #Should I instead raise an exception here?
 			connection.users.append(user)
@@ -73,10 +66,14 @@ async def get_connection_users(conn_id: int, usernames: List[str], current_user:
 	session.commit()
 	return [user for user in connection.users]
 
-@app.put("/{conn_id}", response_model=ConnectionData)
-async def change_connection(conn_id: int, new_data : ConnectionInput, current_user: User = Depends(get_current_user)):
-	current_user.test_security(level=3)
-	connection = get_connection(conn_id, HTTP_404_NOT_FOUND, err_msg="Connection with id='" + str(conn_id) + "' not found")
+@app.post("/{conn_id}/campaigns", response_model=List[str], dependencies=[Depends(AuthUser(3))])
+async def add_campaign_to_connection(conn_id: int, campaign: str, session= Depends(get_db)):
+	connection = get_connection(session, conn_id, HTTP_404_NOT_FOUND, err_msg="Connection with id='" + str(conn_id) + "' not found")
+	return add_campaign(connection, campaign)
+
+@app.put("/{conn_id}", response_model=ConnectionData, dependencies=[Depends(AuthUser(3))])
+async def change_connection(conn_id: int, new_data : ConnectionInput, session= Depends(get_db)):
+	connection = get_connection(session, conn_id, HTTP_404_NOT_FOUND, err_msg="Connection with id='" + str(conn_id) + "' not found")
 	connection_dict = connection.dict()
 	exception = HTTPException(
 		status_code=HTTP_409_CONFLICT,
@@ -99,19 +96,17 @@ async def change_connection(conn_id: int, new_data : ConnectionInput, current_us
 		raise exception
 	return add_local_resource(db= session, Resource= Connection, user_input= new_data, r_id=connection.id)
 
-@app.delete("/{conn_id}", response_model=ConnectionData)
-async def remove_connection(conn_id: int, current_user: Connection = Depends(get_current_user)):
-	current_user.test_security(level=3)
-	connection = get_connection(conn_id, HTTP_404_NOT_FOUND, err_msg="Connection with id='" + str(conn_id) + "' not found")
+@app.delete("/{conn_id}", response_model=ConnectionData, dependencies=[Depends(AuthUser(3))])
+async def remove_connection(conn_id: int, session= Depends(get_db)):
+	connection = get_connection(session, conn_id, HTTP_404_NOT_FOUND, err_msg="Connection with id='" + str(conn_id) + "' not found")
 	return delete_local_resource(db= session, Resource= Connection, instance= connection)
 
-@app.delete("/{conn_id}/users", response_model=List[UserData])
-async def get_connection_users(conn_id: int, usernames: List[str], current_user: User = Depends(get_current_user)):
-	current_user.test_security(level=3)
-	connection = get_connection(conn_id, HTTP_404_NOT_FOUND, err_msg="Connection with id='" + str(conn_id) + "' not found")
+@app.delete("/{conn_id}/users", response_model=List[UserData], dependencies=[Depends(AuthUser(3))])
+async def remove_users_from_connection(conn_id: int, usernames: List[str], session= Depends(get_db)):
+	connection = get_connection(session, conn_id, HTTP_404_NOT_FOUND, err_msg="Connection with id='" + str(conn_id) + "' not found")
 	to_delete = []
 	for username in usernames:
-		user = get_user(username, HTTP_404_NOT_FOUND, err_msg="User '" + username + "' not found")
+		user = get_user(session, username, HTTP_404_NOT_FOUND, err_msg="User '" + username + "' not found")
 		user_exists = [user for user in connection.users if user.username == username]
 		# Should consider raising an exception here if the user is not found
 		if len(user_exists) > 0:
@@ -120,3 +115,7 @@ async def get_connection_users(conn_id: int, usernames: List[str], current_user:
 	session.commit()
 	return to_delete
 
+@app.delete("/{conn_id}/campaigns", response_model=List[str], dependencies=[Depends(AuthUser(3))])
+async def remove_campaign_from_connection(conn_id: int, campaign: str, session= Depends(get_db)):
+	connection = get_connection(session, conn_id, HTTP_404_NOT_FOUND, err_msg="Connection with id='" + str(conn_id) + "' not found")
+	return remove_campaign(connection, campaign)
