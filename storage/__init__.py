315ea32ea4573e4a6da914feabfc2b4fc134b2c9
Antonio <amcbd89@gmail.com>
2019-11-18 18:28:52 +0000

    Updated startup scripts to provide a scoped session variable changed the location of the definition of where database files are located in the filesystem

--- storage/__init__.py
+++ storage/__init__.py
@@ -1,21 +1,34 @@
 import base64
+import os
 
 from pathlib import Path
 #from sqlitedict import SqliteDict
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, scoped_session
 
 from daqbrokerServer.web.utils import hash_password
-from daqbrokerServer.storage.local_settings import Base, User, Connection
+from daqbrokerServer.storage.local_schema import Base, User, Connection, db_folder
+from daqbrokerServer.storage.contextual_session import session_open
 
-local_url = "sqlite+pysqlite:///" + str(Path(__file__).parent / "storage_local.sqlite")
+###### THIS CREATES THE LOCAL STRUCTURE NECESSARY TO HOLD LOCAL DATABASES #######
+if not os.path.isdir(db_folder):
+	os.mkdir(db_folder)
+
+# Initialise the local settings database
+local_url = "sqlite+pysqlite:///" + str(db_folder / "settings.sqlite")
 local_engine = create_engine(local_url)
 
+#################################################################################
+
+# This should create the mappings necessary on the local database
 Base.metadata.reflect(local_engine, extend_existing= True, autoload_replace= False)
 Base.metadata.create_all(local_engine, checkfirst= True)
 
+#This starts a session - probably not ideal, should consider using scoped session
+LocalSession = scoped_session(sessionmaker(bind=local_engine))
 Session = sessionmaker(bind=local_engine)
 session = Session()
+
 
 ######## THIS IS VERY DANGEROUS - IT SHOULD BE A PROMPT CREATED WHEN INSTALLING THE LIBRARY
 query = session.query(User).filter(User.id == 0)
