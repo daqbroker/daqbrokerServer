502cd2193bd8e866da674002d0dcd95be5d9f9d3
Antonio <amcbd89@gmail.com>
2019-12-11 09:40:02 +0000

    Updated routes endpoints and removed unecessary imports

--- web/routes/users/users.py
+++ web/routes/users/users.py
@@ -1,16 +1,16 @@
+import threading
+
 from typing import List
 
 from fastapi import APIRouter
 from starlette.status import HTTP_401_UNAUTHORIZED, HTTP_404_NOT_FOUND, HTTP_409_CONFLICT, HTTP_422_UNPROCESSABLE_ENTITY
-
-from daqbrokerServer.web.routes.utils import get_current_user, Depends, HTTPException, oauth2_scheme, AuthUser
 
 
 from daqbrokerServer.storage.local_schema import User, Connection
 from daqbrokerServer.web.classes.user import User as UserData, UserInput
 from daqbrokerServer.web.classes.connection import Connection as ConnectionData
 from daqbrokerServer.storage.utils import get_local_resources, add_local_resource, delete_local_resource
-from daqbrokerServer.web.routes.utils import get_user, get_connection, get_db
+from daqbrokerServer.web.routes.utils import get_user, get_connection, get_db, get_current_user, Depends, HTTPException, AuthUser
 
 app = APIRouter()
 
@@ -34,7 +34,7 @@ async def add_user(new_user: UserInput, current_user: User = Depends(get_current
 	return add_local_resource(db= session, Resource= User, user_input= new_user)
 
 @app.post("/{username}/connections", response_model=List[ConnectionData], dependencies=[Depends(AuthUser(2))])
-async def add_user_connection(conn_ids: List[int], username: str, current_user: User = Depends(get_current_user), session= Depends(get_db)):
+async def add_user_connections(conn_ids: List[int], username: str, current_user: User = Depends(get_current_user), session= Depends(get_db)):
 	user = get_user(session, username, HTTP_404_NOT_FOUND, err_msg = "User '" + username + "' not found")
 	for conn_id in conn_ids:
 		connection  = get_connection(session, conn_id, HTTP_404_NOT_FOUND, err_msg="Connection with id='" + str(conn_id) + "' not found")
@@ -57,10 +57,11 @@ async def change_user(username: str, new_data : UserInput, current_user: User = 
 		raise exception
 	for attr in to_check: # Maybe put this in another function
 		attr_check = [getattr(user_check, attr) for user_check in users]
-		if getattr(new_data, attr) != getattr(user, attr) and getattr(new_data, attr) in attr_check:
-			status_code=HTTP_409_CONFLICT,
-			exception.detail = attr + " with value '" + getattr(user, attr) + "' already exists"
-			raise exception
+		if getattr(new_data, attr) != getattr(user, attr) and getattr(new_data, attr) in attr_check: 
+			raise HTTPException(
+				status_code = HTTP_409_CONFLICT,
+				detail = attr + " with value '" + getattr(user, attr) + "' already exists"
+			)
 	return add_local_resource(db= session, Resource= User, user_input= new_data, r_id=user.id)
 
 @app.delete("/{username}", response_model=UserData, dependencies=[Depends(AuthUser(3))])
@@ -68,8 +69,8 @@ async def remove_user(username: str, current_user: User = Depends(get_current_us
 	user = get_user(session, username, HTTP_404_NOT_FOUND, err_msg = "User '" + username + "' not found")
 	return delete_local_resource(db= session, Resource= User, instance= user)
 
-@app.delete("/{username}/connections/", response_model=List[ConnectionData], dependencies=[Depends(AuthUser(3))])
-async def remove_user(username: str, conn_ids: List[int], current_user: User = Depends(get_current_user), session= Depends(get_db)):
+@app.delete("/{username}/connections", response_model=List[ConnectionData], dependencies=[Depends(AuthUser(3))])
+async def remove_user_connections(username: str, conn_ids: List[int], current_user: User = Depends(get_current_user), session= Depends(get_db)):
 	user = get_user(session, username, HTTP_404_NOT_FOUND, err_msg = "User '" + username + "' not found")
 	to_delete = []
 	for conn_id in conn_ids:
